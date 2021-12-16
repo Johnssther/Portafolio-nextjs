fba48eae71dc4d9a0dac94be939a313d1e73e4a4
Alejandro Hernández <j.ahr@hotmail.com>
2021-12-16 17:51:05 -0500

    app next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "015634415188de2e"; }
+/******/ 		__webpack_require__.h = function() { return "0788172252368cf4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
